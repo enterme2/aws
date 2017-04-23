556d5fcd182af671c33633a3b272c4c4fdac1abb
enterme2 <lexz.1994@gmail.com>
2017-04-23 14:07:04 +0800

    Add files via upload

---
+++ 23.4.2017/showAll_pMaintenance.php
@@ -0,0 +1,18 @@
+
+ <!DOCTYPE html>
+
+
+<html>
+<head>
+<title>View Preventive Maintenance</title>
+</head>
+<body>
+<center>
+<h1>Preventive Maintenance Work</h1>
+
+<?php
+include('php/viewAll_pMaintenance.php');
+?>
+</center>
+</body>
+</html> 
---
+++ 23.4.2017/showAll_wRequest.php
@@ -0,0 +1,18 @@
+
+ <!DOCTYPE html>
+
+
+<html>
+<head>
+<title>View Work Request</title>
+</head>
+<body>
+<center>
+<h1>Work Requests</h1>
+
+<?php
+include('php/viewAll_wReq.php');
+?>
+</center>
+</body>
+</html> 
